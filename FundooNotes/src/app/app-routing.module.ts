56046057ce39d3aeee9447089ef8ebbe7ef13daf
PoonamGhutukade <poonam.ghutukade@gmail.com>
2019-08-19 19:44:36 +0530

    19August2019

---
+++ FundooNotes/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RegisterComponent } from './component/register/register.component';
+
+import { LoginComponent } from './component/login/login.component';
+
+
+
+const routes: Routes = [
+ 
+ { path: 'register', component:RegisterComponent },
+ { path: 'login', component:LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ FundooNotes/src/app/model/userModel.spec.ts
@@ -0,0 +1,7 @@
+import { UserModel } from 'src/app/model/userModel';
+
+describe('UserModel', () => {
+  it('should create an instance', () => {
+    expect(new UserModel()).toBeTruthy();
+  });
+});
